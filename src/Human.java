fe2117a58af54335507984feab738901ad78b6be
Kuba <zjakub2004@gmail.com>
2022-09-15 14:19:33 +0200

    Abstract

---
+++ src/Human.java
@@ -0,0 +1,6 @@
+public class Human extends Life {
+    @Override
+    void talk() {
+        System.out.println("Talking");  // The body of animalSound() is provided here
+    }
+}
---
+++ src/Life.java
@@ -0,0 +1,8 @@
+public abstract class Life {
+
+    abstract void talk();  // Abstract method (does not have a body)
+
+    void talkMore(){
+        System.out.println("ahniauhfasf");
+    }
+}
